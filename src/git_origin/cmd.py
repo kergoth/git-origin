c9ae482c536c9e3cfe490bcb0ac4e75109ad901f
Chris Larson <clarson@mvista.com>
2009-12-03 10:50:30 -0700

    Rearrange the code a bit, kill an unused function.
    Signed-off-by: Chris Larson <clarson@mvista.com>

--- src/git_origin/cmd.py
+++ src/git_origin/cmd.py
@@ -16,46 +16,6 @@ class Messenger:
     def __init__(self, **kwargs):
         self.__dict__ = kwargs
 
-def _commit(repo, ref="HEAD"):
-    id = repo.git.rev_parse(ref, verify=True).strip()
-    return repo.commit(id)
-
-def _origins(commit):
-    repo = commit.repo
-    notes = _commit(repo, notes_ref)
-    for fn in notes.tree:
-        if fn == commit.id:
-            blob = notes.tree[fn]
-            return [repo.commit(id.strip()) for id in blob.data.splitlines()]
-
-def _add_origin(origin, commit):
-    origin_wd = join(commit.repo.path, "origins-wd")
-    try:
-        makedirs(origin_wd)
-    except OSError, e:
-        if e.errno != EEXIST:
-            raise
-
-    originenv = {
-        "GIT_INDEX_FILE": join(commit.repo.path, "origins-index"),
-        "GIT_WORK_TREE": origin_wd
-    }
-
-    ret = call(["git", "read-tree", notes_ref], env=originenv)
-    if ret != 0:
-        raise Exception("Unable to check out origins.")
-
-    ret = call(["git", "checkout-index", "-af"], env=originenv)
-    if ret != 0:
-        raise Exception("Unable to check out origins.")
-
-def new_blob(repo, data, path=None):
-    args = dict(stdin=True, t="blob", w=True, input=data)
-    if path:
-        args["path"] = path
-    hash = repo.git.hash_object(**args)
-    return repo.blob(hash)
-
 class Index(object):
     def __init__(self, repo, path=None):
         if path is None:
@@ -104,13 +64,26 @@ class Index(object):
     def write_tree(self, *args, **kwargs):
         return self.git.write_tree(*args, **kwargs)
 
+
+def _commit(repo, ref="HEAD"):
+    id = repo.git.rev_parse(ref, verify=True)
+    return repo.commit(id)
+
+def origins(commit):
+    repo = commit.repo
+    notes = _commit(repo, notes_ref)
+    for fn in notes.tree:
+        if fn == commit.id:
+            blob = notes.tree[fn]
+            return [repo.commit(id.strip()) for id in blob.data.splitlines()]
+
 def checkout_origins(repo):
     index = Index(repo, join(repo.path, "origins-index"))
     index.read_tree(notes_ref)
     index.checkout(join(repo.path, "origins-wd"), a=True, f=True)
 
 def add_origin(repo, commit, origin):
-    origins = _origins(commit) or []
+    origins = origins(commit) or []
     originids = [c.id for c in origins]
     if origin.id in originids:
         return
@@ -125,8 +98,9 @@ def add_origin(repo, commit, origin):
     newcommitid = repo.git.commit_tree(newtreeid,
                                        "-p", repo.commit(notes_ref).id,
                                        input="Add origin %s to commit %s" % (neworigin.id, commitid))
-    repo.git.update_ref(notes_ref, newcommitid.strip())
+    repo.git.update_ref(notes_ref, newcommitid)
     return True, origins
+
 
 # Commands
 def origin():
